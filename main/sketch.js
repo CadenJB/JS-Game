c60786b19dca2411db909fafcd57b97cf4bd97ae
CambrianNode <matthew.r.hutt@gmail.com>
2018-04-10 11:12:16 -0400

    Setup main folder. started sketch.js

--- main/sketch.js
+++ main/sketch.js
@@ -1,5 +1,33 @@
+class character {
+  constructor( _grv, _pos, _spd, _vel, _siz ) {
+    this.pos = _pos;
+    this.spd = _spd;
+    this.vel = _vel;
+    this.grv = _grv;
+    this.siz = _siz;
+  }
+  move() {
+    this.pos.add( this.vel );
+    this.vel.add( this.grv );
+  }
+  show() {
+    rect( this.pos.x, this.pos.y, this.pos.x + this.size, this.pos.y + this.size );
+  }
+  update() {
+    this.move();
+    this.show();
+  }
+}
+
+let character1;
+
 function setup() {
-	createCanvas(windowWidth, windowHeight);
+  createCanvas( windowWidth, windowHeight );
+  character1 = new character( cV( 0, 2 ), cV( width / 2, height / 2 ), 2, cV( 0, 0 ), 10 );
+}
+
+function cV( a, b ) {
+  return createVector( a, b );
 }
 
 function draw() {
