1f451731ecd40c0f96d688ff9b86179430738a55
CambrianNode <matthew.r.hutt@gmail.com>
2018-04-11 17:25:13 -0400

    Remade sketch.js. Added TODOs for sprites + sketch.js

--- main/sketch.js
+++ main/sketch.js
@@ -1,1 +1,15 @@
+class character {
+  // TODO: create character class.
+  // TODO: Decide if to use velocity or not.
+  constructor() {
 
+  }
+}
+
+function setup() {
+  createCanvas( 500, 500 );
+}
+
+function draw() {
+  background( 0 );
+}
