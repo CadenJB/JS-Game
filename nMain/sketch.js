8cedaf366bdc5b815e0ed95f735a7ae4850169a6
CambrianNode <matthew.r.hutt@gmail.com>
2018-04-12 18:45:10 -0400

    0.0.3 Pre-Alpha added screen-wrap added movement added rotation

--- nMain/sketch.js
+++ nMain/sketch.js
@@ -1,10 +1,11 @@
 var boxSprites = [];
-var direction;
+
 class character {
   constructor( _x, _y, _w, _h ) {
     this.guy = createSprite( _x, _y, _w, _h );
-    this.guy.maxSpeed = 3;
+    this.guy.maxSpeed = 5;
     this.guy.setSpeed( 0, 0 );
+    this.guy.friction = 0.01;
   }
   update() {
     this.move();
@@ -12,33 +13,44 @@ class character {
   }
   move() {
     if ( keyIsDown( LEFT_ARROW ) ) {
-      this.guy.rotation -= 4;
+      this.guy.rotation -= 3;
     }
     if ( keyIsDown( RIGHT_ARROW ) ) {
-      this.guy.rotation += 4;
-    }
-    if ( keyIsDown( DOWN_ARROW ) ) {
-      this.guy.addSpeed( 2, this.guy.rotation );
+      this.guy.rotation += 3;
     }
     if ( keyIsDown( UP_ARROW ) ) {
-      this.guy.addSpeed( -2, this.guy.rotation );
+      this.guy.addSpeed( -0.1, this.guy.rotation + 90 );
     }
   }
 
 }
 var dude;
 
+function screenWrap() {
+  if ( dude.guy.position.x > width + 32 ) {
+    dude.guy.position.x = -32;
+  }
+  if ( dude.guy.position.y > height ) {
+    dude.guy.position.y = -32;
+  }
+  if ( dude.guy.position.x < -32 ) {
+    dude.guy.position.x = width / 2;
+  }
+  if ( dude.guy.position.y < -32 ) {
+    dude.guy.position.y = height;
+  }
+}
+
 function setup() {
   createCanvas( 800, 600 );
   shipImage = loadImage( "/../sprites/ship.png" );
-
   dude = new character( width / 2, height / 2, 32, 32 );
   dude.guy.addImage( "normal", shipImage );
 }
 
 function draw() {
-  background( 240 );
-  //draw all the sprites
+  background( 47, 50, 59 );
+  screenWrap();
   drawSprites();
   dude.update();
 }
