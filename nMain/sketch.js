3382c1a120e0a59a13e52994edb907061d0e9e56
CambrianNode <matthew.r.hutt@gmail.com>
2018-04-12 21:37:13 -0400

    0.1.0 Pre-Alpha Added ship Added bullet class Added ship logic Added screen wrap Minor bug fixes

--- nMain/sketch.js
+++ nMain/sketch.js
@@ -1,56 +1,69 @@
-var boxSprites = [];
+var ship;
+var bullets = [];
 
 class character {
   constructor( _x, _y, _w, _h ) {
-    this.guy = createSprite( _x, _y, _w, _h );
-    this.guy.maxSpeed = 5;
-    this.guy.setSpeed( 0, 0 );
-    this.guy.friction = 0.01;
+    this.obj = createSprite( _x, _y, _w, _h );
+    this.obj.maxSpeed = 5;
+    this.obj.setSpeed( 0, 0 );
+    this.obj.friction = 0.01;
   }
   update() {
     this.move();
-    this.guy.update();
+    this.obj.update();
   }
   move() {
     if ( keyIsDown( LEFT_ARROW ) ) {
-      this.guy.rotation -= 3;
+      this.obj.rotation -= 3;
     }
     if ( keyIsDown( RIGHT_ARROW ) ) {
-      this.guy.rotation += 3;
+      this.obj.rotation += 3;
     }
     if ( keyIsDown( UP_ARROW ) ) {
-      this.guy.addSpeed( -0.1, this.guy.rotation + 90 );
+      this.obj.addSpeed( -0.1, this.obj.rotation + 90 );
     }
   }
-
 }
-var dude;
+
+class bullet {
+  constuctor( _x, _y, _w, _h ) {
+    this.obj = createSprite( _x, _y, _w, _h );
+    this.obj.maxSpeed( 7 );
+    this.obj.setSpeed( 0, 0 );
+    this.obj.friction( 0 );
+    this.direct = 0;
+  }
+  move( direct, cX, cY ) {
+    this.obj.position( cX, xY );
+    this.obj.setSpeed( 7, direct );
+  }
+}
 
 function screenWrap() {
-  if ( dude.guy.position.x > width + 32 ) {
-    dude.guy.position.x = -32;
-  }
-  if ( dude.guy.position.y > height ) {
-    dude.guy.position.y = -32;
-  }
-  if ( dude.guy.position.x < -32 ) {
-    dude.guy.position.x = width / 2;
-  }
-  if ( dude.guy.position.y < -32 ) {
-    dude.guy.position.y = height;
+  for ( var i = 0; i < allSprites.length; i++ ) {
+    var s = allSprites[ i ];
+    if ( s.position.x < -32 ) s.position.x = width + 32;
+    if ( s.position.x > width + 32 ) s.position.x = -32;
+    if ( s.position.y < -32 ) s.position.y = height + 32;
+    if ( s.position.y > height + 32 ) s.position.y = -32;
   }
 }
 
 function setup() {
   createCanvas( 800, 600 );
   shipImage = loadImage( "/../sprites/ship.png" );
-  dude = new character( width / 2, height / 2, 32, 32 );
-  dude.guy.addImage( "normal", shipImage );
+  bulletImage = loadImage( "/../sprites/bullet.png" );
+  ship = new character( width / 2, height / 2, 32, 32 );
+  for ( var i = 0; i < 10; i++ ) {
+    bullets[ i ] = new bullet( -10, -10, 5, 5 )
+  }
+  ship.obj.addImage( shipImage );
 }
 
 function draw() {
   background( 47, 50, 59 );
   screenWrap();
   drawSprites();
-  dude.update();
+  ship.update();
+  screenWrap();
 }
